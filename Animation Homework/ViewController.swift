2ecf8eb79d3144b8d3d6dd9f7cdf025dd59692b6
Max Marlatt <m.marlatt@setonhill.edu>
2020-02-17 16:16:19 -0500

    Initial Commit

---
+++ Animation Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation Homework
+//
+//  Created by Maxwell Marlatt on 2/17/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
