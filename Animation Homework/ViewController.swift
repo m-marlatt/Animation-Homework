913c5839368d9b28fcd9e152504eac7dcb97cf55
Max Marlatt <m.marlatt@setonhill.edu>
2020-02-18 14:14:21 -0500

    Ready to grade

--- Animation Homework/ViewController.swift
+++ Animation Homework/ViewController.swift
@@ -9,10 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var aniImage: UIImageView!
+    @IBOutlet weak var backImg: UIImageView!
+    @IBOutlet weak var speedSlider: UISlider!
+    
+    
+    
+    
+    @IBAction func strtButt(_ sender: Any) {
+        if (aniImage.isAnimating)
+        {
+            aniImage.stopAnimating()
+        }else{
+            aniImage.startAnimating()
+        }
+    }
+    
+    @IBAction func toggleButton(_ sender: Any) {
+        if (backImg.isHidden == true)
+        {
+            backImg.isHidden = false
+        }else{
+            backImg.isHidden = true
+        }
+    }
+    @IBAction func aniSpeed(_ sender: Any) {
+        aniImage.animationDuration = TimeInterval(1.0-speedSlider.value)
+    }
+    
+        
 
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fishAni: [UIImage] = [
+            UIImage(named:"frame2")!,
+            UIImage(named:"frame3")!,
+            UIImage(named:"frame4")!,
+            UIImage(named:"frame5")!,
+            UIImage(named:"frame6")!,
+            UIImage(named:"frame7")!,
+            UIImage(named:"frame8")!,
+            UIImage(named:"frame9")!,
+            UIImage(named:"frame10")!,
+            UIImage(named:"frame11")!,
+            UIImage(named:"frame12")!,
+         ]
+        aniImage.animationImages = fishAni
+            aniImage.animationDuration=2.5
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
